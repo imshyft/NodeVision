1716c9c0f654e26fe6c502dfef34c052df193ed3
shyft <93425247+imshyft@users.noreply.github.com>
2026-07-26 20:46:00 +1000

    Created avalonia app project

---
+++ src/NodeVision.App/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,6 @@
+﻿namespace NodeVision.App.ViewModels;
+
+public partial class MainWindowViewModel : ViewModelBase
+{
+    public string Greeting { get; } = "Welcome to Avalonia!";
+}
---
+++ src/NodeVision.App/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace NodeVision.App.ViewModels;
+
+public abstract class ViewModelBase : ObservableObject
+{
+}
